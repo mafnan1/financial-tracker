87c59ec9d65e126412ebd77520eecdaf940481c1
afnan-stand8 <afnan.khan@stand8.io>
2023-10-01 03:40:08 +0500

    Initial Commit

---
+++ financial tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  financial tracker
+//
+//  Created by Muhammad Afnan on 01/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ financial tracker/financial_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  financial_trackerApp.swift
+//  financial tracker
+//
+//  Created by Muhammad Afnan on 01/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct financial_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
